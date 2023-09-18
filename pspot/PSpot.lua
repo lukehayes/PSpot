2fee03966d062d5e9c3be55c0cc8c89d83872de1
Luke Hayes <lukepurplehayes@gmail.com>
2023-09-18 11:48:51 +0100

    Corrected ldoc syntax.

--- pspot/PSpot.lua
+++ pspot/PSpot.lua
@@ -1,22 +1,26 @@
 --- PSpot
--- @module PSpot
+-- @classmod PSpot
 local class = require('libs.middleclass')
 
 local PSpot = class('PSpot')
 
 --- Constructor
--- @param number The width of the game window
--- @param number The height of the game window
+-- @param width The width of the game window
+-- @param height The height of the game window
 function PSpot:initialize(width, height)
     self.width = width
     self.height = height
     self.elements = {}
 end
 
+--- The main update() method
+-- @param dt Delta time
 function PSpot:update(dt)
     -- TODO Implmement
 end
 
+--- The main draw() method
+-- @return nil
 function PSpot:draw()
     -- TODO Implmement
 end
