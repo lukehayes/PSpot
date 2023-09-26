4e9355ccb684181575e6341e16cc67f0de4044d1
Luke Hayes <lukepurplehayes@gmail.com>
2023-09-26 10:56:46 +0100

    Implemented util.inside() and util.collides() functions.

--- pspot/util.lua
+++ pspot/util.lua
@@ -3,18 +3,25 @@
 --
 local util = {}
 
-function util:collides(x1,y1,w1,h1,x2,y2,w2,h2)
-    -- TODO Implement
-    print("Collides")
-    print(x1,y1,w1,h1,x2,y2,w2,h2)
-    print("------------------")
+function util.inside(x1,y1,w1,h1,x2,y2,w2,h2)
+    return x1 > x2 and
+           w1 < w2 and
+           y1 > y2 and
+           h1 < h2
 end
 
-function util:collidesObject(a,b)
+function util.collides(x1,y1,w1,h1,x2,y2,w2,h2)
+    return x1 > x2 or
+           w1 < w2 or
+           y1 > y2 or
+           h1 < h2
+end
+
+function util.collidesObject(a,b)
     -- TODO Implement
     print("Collides Object")
     print(a.x,a.y,a.w,a.h,b.x,b.y,b.w,b.h)
     print("------------------")
 end
 
-return utl
+return util
